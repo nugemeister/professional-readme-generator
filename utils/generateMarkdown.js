ef4fa3dea211444d94c8d3c179760f0b78ef9573
Ali Nugen <alexandra.nugen@gmail.com>
2022-08-11 23:01:42 -0700

    updating markdown file

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,9 +1,38 @@
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseBadge(license) {
+  if(license === "Apache2.0") {
+    return "License-Apache_2.0-blue.svg"
+  } else if(license === "GPLv2-blue.svg") {
+    return "License-GPLv2-blue.svg"
+  } else if(license === "MIT") {
+    return "License-MIT-yellow.svg"
+  } else {
+    return ""
+  }
+}
 
-function renderTitle(title) {
-  return `# ${data.title}`;
+function renderLicenseLink(license) {
+  if(license === "None") {
+    return ""
+  } else if(license === "Other") {
+    return ""
+  } else {
+    return license
+  }
+}
+
+function renderLicenseSection(license) {
+  if(license === "None") {
+    return "";
+  } else if(license === "Other") {
+    return "";
+  } else return `
+## License:
+[![License](https://img.shields.io/badge/${renderLicenseBadge(license)})](https://opensource.org/licenses/${renderLicenseLink(license)})
+  
+---
+`
 }
 
 // TODO: Create a function to generate markdown for README
@@ -11,9 +40,13 @@ function generateMarkdown(data) {
   console.log("generateMarkdown is running!");
   renderLicenseBadge(data);
   renderLicenseLink(data);
-  return `# ${data.title}`;
+  return `
+    # ${data.title};
+    $ ${data.description}
+
+
+  `; 
 }
 
-${licenseBadge}${renderLicenseLink}
 
 module.exports = generateMarkdown;
